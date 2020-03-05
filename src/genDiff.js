ce2becb64d2cd24f7e8c14b207c4bfc3507241cc
Make Love <makelove@kk.hk>
2020-03-05 10:32:44 +0300

    fix

--- src/genDiff.js
+++ src/genDiff.js
@@ -8,15 +8,15 @@ export default (path1, path2) => {
   const data2Entries = Object.entries(parsedData2);
   const changedAndRemoved = data1Entries.reduce((acc, el) => {
     const [key, value] = el;
-    if (_.has(parcedData2, `${key}`)) {
-      return parcedData2[key] === value ? `${acc}  ${key}: ${value}\n`
+    if (_.has(parsedData2, `${key}`)) {
+      return parsedData2[key] === value ? `${acc}  ${key}: ${value}\n`
         : `${acc}+ ${key}: ${parcedData2[key]}\n- ${key}: ${value}\n`;
     }
     return `${acc}- ${key}: ${value}\n`;
   }, '');
   const added = data2Entries.reduce((acc, el) => {
     const [key, value] = el;
-    if (!_.has(parcedData1, `${key}`)) {
+    if (!_.has(parsedData1, `${key}`)) {
       return `${acc}+ ${key}: ${value}\n`;
     }
     return acc;
