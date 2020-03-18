a982606a5c8774579aea0d15ae74b4562c7fa934
Make Love <makelove@kk.hk>
2020-03-18 15:30:43 +0300

    fix

--- src/formatInterface.js
+++ src/formatInterface.js
@@ -4,7 +4,7 @@ import plainDiff from './formatters/plainDiff.js';
 export default class FormatInterface {
   constructor(path1, path2) {
     this.path1 = path1;
-    this.path2 = path2;    
+    this.path2 = path2;
   }
 
   objective() {
